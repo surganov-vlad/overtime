4f8a453aae755a559da62311d178bf80b2b2b312
Vladimir Surganov <surganov.vlad@gmail.com>
2016-10-05 19:31:46 -0500

    added new post button to nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,6 +26,14 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the nav bar' do
+      visit root_path
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
