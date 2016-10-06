8b60eaa5e6f9c453d1c2b3b3e17ec5bd1de5b79c
Vladimir Surganov <surganov.vlad@gmail.com>
2016-10-05 19:55:31 -0500

    styled posts form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -24,10 +24,15 @@ class PostsController < ApplicationController
 
   def update
     @post.update(post_params)
-    redirect_to @post, notice: 'Your post was created successfully.'
+    redirect_to @post, notice: 'Your post was edited successfully.'
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
   end
 
   private
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -84,4 +84,14 @@ describe 'navigate' do
       expect(User.last.posts.last.rationale).to eq("Edited Content")
     end
   end
+
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
 end
